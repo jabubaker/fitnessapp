766bc360c14e7d8d7f6810b339e9162b022b17c7
Jisha Abubaker <jisha.abubaker@gmail.com>
2024-12-04 18:02:20 -0800

    fix weight rendering

--- src/components/WorkoutDisplay.js
+++ src/components/WorkoutDisplay.js
@@ -5,16 +5,22 @@ function WorkoutDisplay({ exercise, completedSets, isTimerRunning }) {
     <div className="workout-display">
       <h2>{exercise.exercise.toUpperCase()}</h2>
       <p>
-        {exercise.reps} reps x {exercise.sets} sets @ {exercise.weight} lbs
+        {exercise.reps} reps x {exercise.sets} sets{renderWeight(exercise.weight)}
       </p>
       <p>
-        {isTimerRunning ? 
-          `Completed Sets: ${completedSets + 1}/${exercise.sets}` :
-          `Current Set: ${completedSets + 1}/${exercise.sets}`
-        }
+        {isTimerRunning
+          ? `Completed Sets: ${completedSets + 1}/${exercise.sets}`
+          : `Current Set: ${completedSets + 1}/${exercise.sets}`}
       </p>
     </div>
   );
+}
+
+function renderWeight(weight) {
+  if (weight > 0.5) {
+    return `. Use ${weight.toString()} lbs.`; // Correct interpolation
+  }
+  return ''; // Return an empty string if weight is <= 0.5
 }
 
 export default WorkoutDisplay;
